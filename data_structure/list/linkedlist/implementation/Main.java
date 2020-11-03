cbe253dd3954509ba47db116c71f8be1bf677cd3
munjongjin <dmer11@naver.com>
2020-11-03 17:07:35 +0900

    LinkedList_Version2

--- data_structure/list/linkedlist/implementation/Main.java
+++ data_structure/list/linkedlist/implementation/Main.java
@@ -6,7 +6,23 @@ public class Main {
 		// TODO Auto-generated method stub
 		
 		LinkedList numbers = new LinkedList();
-		LinkedList numbers1 = new LinkedList();
+		
+		
+		numbers.addLast(10);
+		numbers.addLast(20);
+		numbers.addLast(30);
+		numbers.addFirst(5);
+		numbers.add(1, 15);
+		
+		
+		System.out.println( numbers.removeLast() );
+		System.out.println( numbers.size() );
+		System.out.println( numbers.get(2) );
+		System.out.println( numbers ); 
+		System.out.println(numbers.indexOf(5));
 	}
+	
+	
+	
 
 }
